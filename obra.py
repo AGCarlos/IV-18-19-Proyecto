351a9516b4efa8710db904cd2d0245466451d056
MaikelToledo <migueltoledoweb@gmail.com>
2018-10-24 11:49:18 +0200

    Actualizado Proyecto

--- obra.py
+++ obra.py
@@ -1,11 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Clase Obra
+# Se define una obra con su id, nombre, precio, descripción, si está activa, tipo de pago,
+# materiales usados y horas de mano de obra.
+# Autor: Francisco Miguel Toledo Aguilera
+# 
 
-class Obra:
-
-    def __init__(self, n, p, d, a):
+class Obra:                
+    def Nueva(self, i, n, p, d, a, tp, m, hmo):
+        self.id = i
         self.nombre = n
         self.precio = p
         self.descripcion = d
         self.activa = a
+        self.tipo_pago = tp
+        self.materiales = m
+        self.horas_mano_obra = hmo
+
+    def __init__(self):
+        self.id = -1
+        self.nombre = " "
+        self.precio = -1
+        self.descripcion = " "
+        self.activa = False
+        self.tipo_pago = " "
+        self.materiales = " "
+        self.horas_mano_obra = -1
      
     def print_nombre(self):
         print (self.nombre)
@@ -19,6 +39,72 @@ class Obra:
     def print_activa(self):
         print (self.activa)
 
+    def print_tipo_pago(self):
+        print (self.tipo_pago)
+
+    def print_materiales(self):
+        print (self.materiales)
+
+    def print_horas_mano_obra(self):
+        print (self.horas_mano_obra)
+
     def precio_con_iva(self, t):
         iva = (self.precio*t) + self.precio
         return iva
+
+#
+# Clase ObraManager
+# Sistema gestor de Obras (clase anterior)
+# Se define una lista de obras
+# Permite hacer: altas, bajas y borrado
+# 
+
+class ObraManager:
+    def __init__(self):
+        self.lista_obras = []
+
+    def add_obra(self, obra):
+        self.lista_obras.append(obra)
+
+    def buscar_obra(self, obra_id):
+        for ob in self.lista_obras:
+            if ob.id == obra_id:
+                return ob
+            else:
+                print("No se ha encontrado la obra")
+
+    def delete_obra(self, obra_id):
+        index = self.buscar_obra(obra_id)
+        if index != None:
+            self.lista_obras.remove(index)
+            print("Obra borrada con exito")
+        else:
+            print("No se ha encontrado la obra a borrar")
+
+    def num_obras(self):
+        count = 0
+        for ob in self.lista_obras:
+            count +=1
+
+        return count
+
+    def gasto_total_obras_sinIVA(self):
+        count = 0
+        for ob in self.lista_obras:
+            count += ob.precio
+
+        return count
+
+    def gasto_total_obras_conIVA(self):
+        count = 0
+        for ob in self.lista_obras:
+            count += ob.precio_con_iva(0.21)
+
+        return count
+        
+    def horas_manoObra_totales(self):
+        count = 0
+        for ob in self.lista_obras:
+            count += ob.horas_mano_obra
+
+        return count
--- test.py
+++ test.py
@@ -1,7 +1,44 @@
+# coding=utf-8
+# Funciones de testeo para la clase Obra y ObraManager
 
-from obra import Obra
+from obra import Obra, ObraManager
 
-nevada = Obra("Centro Comercial Nevada", 20, "Obra realizada en Granada", True)
+ikea = Obra()
+ikea.Nueva(1, "Ikea", 200500, "Obra realizada en Málaga", True, "Transferencia", "2000m2 de cubierta sandwich", 200)
+leroy = Obra()
+leroy.Nueva(2, "Leroy", 589663, "Obra realizada en Sevilla", False, "Tarjeta de crédito", "5 Fachada sandwich y 10 cubiertas deck", 600)
+nevada = Obra()
+nevada.Nueva(3, "Centro comercial Nevada", 8966524, "Obra realizada en Granada", True, "PayPal", "Suelo laminado y cubierta deck", 1200)
+plazaMayor = Obra()
+plazaMayor.Nueva(3, "Complejo Plaza Mayor", 99966524, "Obra realizada en Málaga", True, "Transferencia", "199925 m2 de azulejos laminados y 859658m2 cubiertas con lucesnarios", 1800)
 
+gestionObras = ObraManager()
+gestionObras.add_obra(ikea)
+gestionObras.add_obra(leroy)
+gestionObras.add_obra(nevada)
+gestionObras.add_obra(plazaMayor)
+
+# Test de precio con iva para una obra individual
 def test_iva():
-    assert nevada.precio_con_iva(0.21) == 24.2
+    assert ikea.precio_con_iva(0.21) == 242605.0
+
+# Test para el nombre de una obra individual
+def test_print_nombre():
+    assert ikea.nombre == "Ikea"
+
+# Test para el numero de obras gestionadas
+def test_num_obras():
+    assert gestionObras.num_obras() == 4
+
+# Test para el gasto total de obras sin IVA
+def test_gasto_total_obras_sinIVA():
+    assert gestionObras.gasto_total_obras_sinIVA() == 109723211
+
+# Test para el gasto total de obras con IVA
+def test_gasto_total_obras_conIVA():
+    assert gestionObras.gasto_total_obras_conIVA() == 132765085.30999999
+
+# Test para el numero de horas de mano de obra totales
+# de todas las obras gestionadas
+def test_horas_manoObra_totales():
+    assert gestionObras.horas_manoObra_totales() == 3800
