5b8b1304df82cf28d05d25e25f728ae1c175b731
MaikelToledo <migueltoledoweb@gmail.com>
2018-11-18 21:03:44 +0100

    Actualizado despliegue Docker en Heroku

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, json
 from datetime import datetime
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/status")
 def status():
     with open('data/status.json') as f:
+        data = json.load(f)
+        response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/datosObras")
+def datos():
+    datos = {
+        "app": "ObrasMTA",
+        "datos": "/datosObras/obraX",
+        "x": "Entero 1 o 2"
+    }
+
+    with open(datos) as f:
         data = json.load(f)
         response = app.response_class(
         response=json.dumps(data),
@@ -25,4 +42,4 @@ def datosObras(file):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True, host='0.0.0.0', port="80")
